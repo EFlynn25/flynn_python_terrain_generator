a3c042bb3774f13ae9975db7a5ebfa41bbec93c9
Everett Flynn <everettflynn25@gmail.com>
2024-07-11 19:33:28 -0700

    Bug fix

--- generate_chunk.py
+++ generate_chunk.py
@@ -11,7 +11,7 @@ def generate_chunk(seed, chunk_x, chunk_y, size, hill_frequency=3, hill_height=5
         y += chunk_y * size
         for x in range(size):
             x += chunk_x * size
-            my_val = (tmp.noise2d(x=hill_frequency*x/200, y=hill_frequency*y/200) + 1) * (hill_height * 3) + (60 - (hill_height * 3))
+            my_val = (tmp.noise2(x=hill_frequency*x/200, y=hill_frequency*y/200) + 1) * (hill_height * 3) + (60 - (hill_height * 3))
             noise_array.append(my_val)
 
     return {"x": x_array, "y": y_array, "noise": noise_array}
